ccc5c6d9d6266882799c2412f497f4ee78c2907a
Zanderstorm99999 <143282957+Zanderstorm99999@users.noreply.github.com>
2024-01-12 12:28:24 -0600

    made backround and "outline" of player appear

--- platformer.py
+++ platformer.py
@@ -8,15 +8,74 @@ pygame.init()
 
 pygame.display.set_caption("Zanderformer")
 
-BG_COLOR = (255, 255, 255)
 WIDTH, HEIGHT = 1000, 800
 FPS = 60
-PLAYER_VELOCITY = 5
+PLAYER_VEL = 5
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
+
+class Player(pygame.sprite.Sprite):
+    COLOR = (255, 0, 0)
+   
+    def __init__(self, x, y, image_width, image_height):
+        self.rect = pygame.Rect(x, y, image_width, image_height)
+        self.x_vel = 0
+        self.y_vel = 0
+        self.mask = None
+        self.direction = "left"
+        self.animation_count = 0
+
+    def move(self, dx, dy):
+        self.rect.x += dx
+        self.rect.y += dy
+
+    def move_left(self, vel):
+        self.x_vel = -vel
+        if self.direction != "left":
+            self.direction = "left"
+            self.animation_count = 0
+
+    def move_right(self, vel):
+        self.x_vel = vel
+        if self.direction != "right":
+            self.direction = "right"
+            self.animation_count = 0
+
+    def loop(self, fps):
+        self.move(self.x_vel, self.y_vel)
+
+    def draw(self, win):
+        pygame.draw.rect(win, self.COLOR, self.rect)
+
+
+def get_background(name):
+    image = pygame.image.load(join("assets", "Background", name))
+    _, _, image_width, image_height = image.get_rect()
+    tiles = []
+
+    for i in range(WIDTH // image_width + 1):
+        for j in range(HEIGHT // image_height + 1):
+            pos = (i * image_width, j * image_height)
+            tiles.append(pos)
+
+    return tiles, image
+
+
+def draw(window, tiles, bg_image, player):
+    for tile in tiles:
+        window.blit(bg_image, tile)
+
+    player.draw(window)
+
+    pygame.display.update()
+
+
 def main(window):
     clock = pygame.time.Clock()
+    tiles, bg_image = get_background("Blue.png")
+
+    player = Player(100, 100, 50, 50)
 
     run = True
     while run:
@@ -27,6 +86,8 @@ def main(window):
                 run = False
                 break
     
+        draw(window, tiles, bg_image, player)
+
     pygame.quit()
     quit()
 
