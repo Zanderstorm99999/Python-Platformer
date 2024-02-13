748a27dcc957ba107e9303109eba1e5b50a9a005
Zanderstorm99999 <143282957+Zanderstorm99999@users.noreply.github.com>
2024-02-12 19:43:13 -0600

    Added block and animation movement and speed. Added type annotation to some functions. Added gravity.

--- platformer.py
+++ platformer.py
@@ -4,6 +4,7 @@ import math
 import pygame
 from os import listdir
 from os.path import isfile, join
+from pygame.surface import Surface
 pygame.init()
 
 pygame.display.set_caption("Zanderformer")
@@ -14,42 +15,123 @@ PLAYER_VEL = 5
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
+def flip(sprites):
+    return[pygame.transform.flip(sprite, True, False) for sprite in sprites]
+
+def load_sprite_sheets(dir1, dir2, width: int, height: int, direction: bool = False) -> dict:
+    path = join("assets", dir1, dir2)
+    images = [f for f in listdir(path) if isfile(join(path, f))]
+
+    all_sprites = {}
+
+    for image in images:
+        sprite_sheet = pygame.image.load(join(path, image)).convert_alpha()
+
+        sprites = []
+        for i in range(sprite_sheet.get_width() // width):
+            surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)
+            rect = pygame.Rect(i * width, 0, width, height)
+            surface.blit(sprite_sheet, (0, 0), rect)
+            sprites.append(pygame.transform.scale2x(surface))
+
+            if direction:
+                all_sprites[image.replace(".png", "") + "_right"] = sprites
+                all_sprites[image.replace(".png", "") + "_left"] = flip(sprites)
+            else:
+                all_sprites[image.replace(".png", "")] = sprites
+    
+    return all_sprites
+
+def get_block(size):
+    path = join("assets", "Terrain", "Terrain.png")
+    image =  pygame.image.load(path).convert_alpha()
+    surface = pygame.Surface((size, size), pygame.SRCALPHA, 32)
+    rect = pygame.Rect(96, 0, size, size)
+    surface.blit(image, (0, 0), rect)
+    return pygame.transform.scale2x(surface)
 
 class Player(pygame.sprite.Sprite):
     COLOR = (255, 0, 0)
+    GRAVITY = 0
+    SPRITES = load_sprite_sheets("MainCharacters", "NinjaFrog", 32, 32, True)
+    ANIMATION_DELAY = 5
    
-    def __init__(self, x, y, image_width, image_height):
+    def __init__(self, x: int, y: int, image_width: int, image_height: int):
+        super().__init__()
         self.rect = pygame.Rect(x, y, image_width, image_height)
         self.x_vel = 0
         self.y_vel = 0
         self.mask = None
         self.direction = "left"
         self.animation_count = 0
+        self.fall_count = 0
 
     def move(self, dx, dy):
         self.rect.x += dx
         self.rect.y += dy
 
-    def move_left(self, vel):
+    def move_left(self, vel: int):
         self.x_vel = -vel
         if self.direction != "left":
             self.direction = "left"
             self.animation_count = 0
 
-    def move_right(self, vel):
+    def move_right(self, vel: int):
         self.x_vel = vel
         if self.direction != "right":
             self.direction = "right"
             self.animation_count = 0
 
-    def loop(self, fps):
+    def loop(self, fps: int):
+        self.y_vel += min(1, (self.fall_count / fps) * self.GRAVITY)
         self.move(self.x_vel, self.y_vel)
 
+
+        self.fall_count += 1
+        self.update_sprite()
+
+    def update_sprite(self):
+        sprite_sheet = "idle"
+        if self.x_vel != 0:
+            sprite_sheet = "run"
+            
+        sprite_sheet_name = sprite_sheet + "_" + self.direction
+        sprites = self.SPRITES[sprite_sheet_name]
+        sprite_index = (self.animation_count // self.ANIMATION_DELAY) % len(sprites)
+        self.sprite = sprites[sprite_index]
+        self.animation_count += 1
+        self.update()
+
+    def update(self): #He didn't put self in the (). Timestamp 56:00 minutes into the video
+        self.rect = self.sprite.get_rect(topleft = (self.rect.x, self.rect.y))
+        self.mask = pygame.mask.from_surface(self.sprite)
+
     def draw(self, win):
-        pygame.draw.rect(win, self.COLOR, self.rect)
+        win.blit(self.sprite, (self.rect.x, self.rect.y))
 
 
-def get_background(name):
+class Object(pygame.sprite.Sprite):
+    def __init__(self, x: int, y: int, width: int, height:int, name = None):
+        super().__init__()
+        self.rect = pygame.Rect(x, y, width, height)
+        self.image = pygame.Surface((width, height), pygame.SRCALPHA)
+        self.width = width
+        self.width = height
+        self.name = name
+
+    def draw(self, win):
+        win.blit(self.image, (self.rect.x, self.rect.y))
+
+        
+class Block(Object):
+    def __init__(self, x, y, size):
+        super().__init__(x, y, size, size)
+        block = get_block(size)
+        self.image.blit(block, (0, 0))
+        self.mask = pygame.mask.from_surface(self.image)
+
+
+def get_background(name) -> tuple:
     image = pygame.image.load(join("assets", "Background", name))
     _, _, image_width, image_height = image.get_rect()
     tiles = []
@@ -62,9 +144,12 @@ def get_background(name):
     return tiles, image
 
 
-def draw(window, tiles, bg_image, player):
+def draw(window: Surface, tiles: list, bg_image, player, objects):
     for tile in tiles:
         window.blit(bg_image, tile)
+
+    for obj in objects:
+        obj.draw(window)
 
     player.draw(window)
 
@@ -80,11 +165,14 @@ def handle_move(player: Player):
     if keys[pygame.K_RIGHT]:
         player.move_right(PLAYER_VEL)
 
-def main(window):
+def main(window: Surface) -> None:
     clock = pygame.time.Clock()
     tiles, bg_image = get_background("Blue.png")
 
+    block_size = 96
+
     player = Player(100, 100, 50, 50)
+    blocks = [Block(0, HEIGHT - block_size, block_size)]
 
     run = True
     while run:
@@ -97,7 +185,7 @@ def main(window):
     
         player.loop(FPS)
         handle_move(player)
-        draw(window, tiles, bg_image, player)
+        draw(window, tiles, bg_image, player, blocks)
 
     pygame.quit()
     quit()
